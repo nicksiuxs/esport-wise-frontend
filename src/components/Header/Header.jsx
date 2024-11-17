330566f8de21704afbe0e6c17f61f08a9b9f812c
Nicolas Londono <nicksiuxs@gmail.com>
2024-11-17 17:37:04 -0500

    Add: cehckpoint header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import useUserStore from "../../store/useUserStore";
+
+const Header = () => {
+  const user = useUserStore((state) => state.user);
+  console.log(user);
+  return (
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <span className="text-sm/6 font-semibold text-gray-900">
+              Esport Wise App
+            </span>
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Equipos
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Jugadores
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Torneos
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="/login/" className="text-sm/6 font-semibold text-gray-900">
+            Iniciar sesión
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/MyTeam/MyTeam.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyTeam = () => {
+  return (
+    <main>
+      <h1>My Team</h1>
+      <p>This space is for follow your team</p>
+    </main>
+  );
+};
+
+export default MyTeam;
